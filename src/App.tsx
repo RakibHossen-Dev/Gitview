558baf8354c82e937e640b7ae5fef666bfd1c015
Rakib <hellorakibhossen@gmail.com>
2025-04-06 21:25:58 +0600

    Environment setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <>
+      <h3 className="text-2xl font-bold text-gray-900">GitView</h3>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MainLayout.tsx
@@ -0,0 +1,5 @@
+const MainLayout = () => {
+  return <div>Ths si main layout</div>;
+};
+
+export default MainLayout;
